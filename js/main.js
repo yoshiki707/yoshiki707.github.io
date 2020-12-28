015d3bb22aa344c0c4fb8e754573b6a30972a904
Yoshiki Murakami <veb.brang@gmail.com>
2020-12-28 15:07:56 +0900

    nullチェックの追加

--- js/main.js
+++ js/main.js
@@ -4,17 +4,21 @@ var list = document.getElementById('#todoLists');
 var createTodoList = function (task) {
     // HTML テンプレートを生成
     var html = "\n  <li>\n    <span>" + task + "</span>\n    <i class=\"far fa-trash-alt delete\"></i>\n  </li>\n  ";
-    list.innerHTML += html;
-};
-addTask.addEventListener('submit', function (e) {
-    // デフォルトのイベントを無効
-    e.preventDedault();
-    // タスクに入力した値を空白を除外して格納
-    var task = addTask.add.value.trim();
-    if (task.length) {
-        // Todo List の HTML を作成
-        createTodoList(task);
-        // タスクに入力した文字をクリア
-        addTask.reset();
+    if (list) {
+        list.innerHTML += html;
     }
-});
+};
+if (addTask) {
+    addTask.addEventListener('submit', function (e) {
+        // デフォルトのイベントを無効
+        e.preventDedault();
+        // タスクに入力した値を空白を除外して格納
+        var task = addTask.add.value.trim();
+        if (task.length) {
+            // Todo List の HTML を作成
+            createTodoList(task);
+            // タスクに入力した文字をクリア
+            addTask.reset();
+        }
+    });
+}
--- js/main.ts
+++ js/main.ts
@@ -10,19 +10,22 @@ const createTodoList = task => {
   </li>
   `;
 
-  list.innerHTML += html;
-}
-
-addTask.addEventListener('submit', e => {
-  // デフォルトのイベントを無効
-  e.preventDedault();
-
-  // タスクに入力した値を空白を除外して格納
-  const task = addTask.add.value.trim();
-  if(task.length) {
-    // Todo List の HTML を作成
-    createTodoList(task);
-    // タスクに入力した文字をクリア
-    addTask.reset();
+  if(list) {
+    list.innerHTML += html;
   }
-})
+}
+if(addTask) {
+  addTask.addEventListener('submit', e => {
+    // デフォルトのイベントを無効
+    e.preventDedault();
+  
+    // タスクに入力した値を空白を除外して格納
+    const task = addTask.add.value.trim();
+    if(task.length) {
+      // Todo List の HTML を作成
+      createTodoList(task);
+      // タスクに入力した文字をクリア
+      addTask.reset();
+    }
+  })
+}
