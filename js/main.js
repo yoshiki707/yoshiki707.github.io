7dac1169bef2efff086428e9b3959b2e567c3642
Yoshiki Murakami <veb.brang@gmail.com>
2020-12-29 15:26:37 +0900

    jsの追加

--- js/main.js
+++ js/main.js
@@ -20,8 +20,25 @@ addTask.addEventListener('submit', function (e) {
         addTask.reset();
     }
 });
+// 削除機能
 list.addEventListener('click', function (e) {
     if (e.target.classList.contains('delete')) {
         e.target.parentElement.remove();
     }
+});
+// フィルタリング機能
+var search = document.getElementById('searchBox');
+var filterTasks = function (term) {
+    Array.form(list.children)
+        // フィルタ条件
+        .filter(function (todo) { return !todo.textContent.toLowerCase().includes(term); })
+        .forEach(function (todo) { return todo.classList.add('filtered'); });
+    Array.form(list.children)
+        .filter(function (todo) { return todo.textContent.toLowerCase().includes(term); })
+        .forEach(function (todo) { return todo.classList.remove('filtered'); });
+};
+search.addEventListener('keyup', function () {
+    // 空白削除かつ、小文字に変換（大文字、小文字の区別をなくす）
+    var term = search.value.trim().toLowerCase();
+    filterTasks(term);
 });
--- js/main.ts
+++ js/main.ts
@@ -27,8 +27,29 @@ addTask.addEventListener('submit', (e) => {
     addTask.reset();
   }
 })
+// 削除機能
 list.addEventListener('click', (e) => {
   if (e.target.classList.contains('delete')) {
     e.target.parentElement.remove();
   }
 })
+// フィルタリング機能
+const search = (document.getElementById('searchBox') as HTMLInputElement);
+
+const filterTasks = (term: string) => {
+
+  Array.form(list.children)
+  // フィルタ条件
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add('filtered'));
+
+  Array.form(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove('filtered'));
+};
+
+search.addEventListener('keyup', () => {
+  // 空白削除かつ、小文字に変換（大文字、小文字の区別をなくす）
+  const term = search.value.trim().toLowerCase();
+  filterTasks(term);
+});
